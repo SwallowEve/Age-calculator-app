10710d9fa2231ca43c2a608972e68832ce7cede0
SwallowEve <swalloweve@gmail.com>
2023-08-23 12:42:20 +0300

    Age-calculation app: styles added to form and wrapper

--- app/components/FormWrap.tsx
+++ app/components/FormWrap.tsx
@@ -2,13 +2,13 @@ import Form from "./Form"
 
 export default function FormWrap() {
     return(
-        <div className="z-10 max-w-5xl w-7/12 place-self-center mt-10 max-h-fit rounded-2xl bg-white wrap-corner">
+        <div className="z-10 w-max place-self-center mt-10 max-h-fit rounded-2xl bg-white wrap-corner">
             <Form></Form>
-            <hr className="m-11 mt-10 h-0.5 bg-slate-100 "></hr>
-            <div className="ml-11 text-5xl tracking-tight font-black italic">
-            <p className=" text-black">years</p>
-            <p className=" text-black">months</p>
-            <p className=" text-black">days</p>
+            <hr className="m-5 md:m-8 mt-14 md:mt-9 h-0.2 bg-slate-100 "></hr>
+            <div className="m-5 md:m-8 mt-10 text-5xl md:text-6xl tracking-tight font-black italic mb-10">
+            <p className=" text-black"><span id="pYear" className="text-indigo-600">--</span> years</p>
+            <p className=" text-black"><span id="pMonth" className="text-indigo-600">--</span> months</p>
+            <p className=" text-black"><span id="pDay" className="text-indigo-600">--</span> days</p>
             </div>
         </div>
     )
